d95fec2cebe146ba0fd8b8576fb60aa055422c7f
Max DePriest <maxd924@gmail.com>
2015-07-21 08:33:58 -0500

    porting over my styling from other app

---
+++ app/controllers/pairs_controller.rb
@@ -0,0 +1,24 @@
+class PairsController < ApplicationController
+
+	def index
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+
+	end
+
+	def update
+	end
+
+	def destroy
+
+	end
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+pairs = Pair.create([{ food: 'Horse Meat', beer: 'Mead', beer_type: 'Strong Ale'}])
+
+pairs2 = Pair.create([{ food: 'Poached Egg', beer: "Woodhouse's Tears", beer_type: 'Blonde Ale'}])
+
+pairs3 = Pair.create([{ food: 'Dirt', beer: 'Mud', beer_type: 'Porter'}])
+
+pairs4 = Pair.create([{ food: 'Crumpets', beer: 'Tea', beer_type: 'Barley Wine'}])
+
+pairs5 = Pair.create([{ food: 'Roast Beast', beer: 'Mead', beer_type: 'Strong Ale'}])
+
+pairs6 = Pair.create([{ food: 'Salad', beer: 'Peter Cotton Ale', beer_type: 'Pale Ale'}])
+
+pairs7 = Pair.create([{ food: 'Raw Steak', beer: 'Sex Panther', beer_type: 'Porter'}])
